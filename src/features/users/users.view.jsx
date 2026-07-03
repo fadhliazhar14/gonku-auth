f01aef99391ed2d0df97bf87435d867410a74c05
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-07-03 22:30:25 +0700

    Remove warning from users view

--- src/features/users/users.view.jsx
+++ src/features/users/users.view.jsx
@@ -65,12 +65,12 @@ export default function UsersView() {
 
                     <button
                         type="submit"
-                        className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >Search
                     </button>
                     <button
                         onClick={() => handleNavigateToDetail(0)}
-                        className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >Add
                     </button>
                 </div>
