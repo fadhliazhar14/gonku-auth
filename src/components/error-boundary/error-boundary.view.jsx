6e70d2943fa9d1d49cdebf5c3d7fdc110a14ac63
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-07-08 15:57:15 +0700

    fix(error-boundary): remove debug console.log from ErrorBoundary

--- src/components/error-boundary/error-boundary.view.jsx
+++ src/components/error-boundary/error-boundary.view.jsx
@@ -3,7 +3,6 @@ import Button from "../forms/button";
 
 export default function ErrorBoundary() {
   const error = useRouteError();
-  console.log(error);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-[400px] p-6 text-center bg-gray-50 rounded-lg border-2 border-dashed border-gray-200">
